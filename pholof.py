dd86897a0499b50a0d53919ecaf8252b15c17e71
ktleary <kevin@ktleary.com>
2020-06-29 15:50:24 -0400

    add lowercase extension comparison, TestFileOperations unit

--- pholof.py
+++ pholof.py
@@ -1,15 +1,10 @@
+import os
 import exiftool
 
 exts = [".jpg", ".gif", ".png", ".jpeg"]
 tags = []
 
-# def listFiles(directory, patterns="*.*"):
-#     filepaths = []
-#     for ROOT, DIR, FILES in os.walk(directory):
-#         for file in FILES:
-#             if file.endswith((tuple(patterns))):
-#                 filepaths.append(os.path.join(ROOT, file))
-#     return sorted(filepaths)
+
 
 
 # def processFiles(filelist):
@@ -19,8 +14,8 @@ tags = []
 #             text = pdf2text(filepath)
 #     return text
 
-def listFiles(directory, patterns):
-	return []
+# def listFiles(directory, patterns):
+# 	return []
 
 
 def findExifFiles(dirs, tags, exts):
--- test/test_file_operations.py
+++ test/test_file_operations.py
@@ -1,19 +1,41 @@
 import unittest
-import pholof
+import utils
+import json
+import os
+
+files = [
+    'Geneva.JPG', 'RANDOM.GIF', 'geneva-comp.jpg', 'lorem.html', 'lorem.md',
+    'lorem.odt', 'lorem.pdf', 'lorem.txt', 'new-hampshire-comp.jpg',
+    'new-york-comp.jpg', 'random.gif', 'random.jpeg', 'randompng-comp.png',
+    'vietnam-comp.jpg', 'vietnam.png'
+]
+
+imageFiles = [
+    'Geneva.JPG', 'RANDOM.GIF', 'geneva-comp.jpg', 'new-hampshire-comp.jpg',
+    'new-york-comp.jpg', 'random.gif', 'random.jpeg', 'randompng-comp.png',
+    'vietnam-comp.jpg', 'vietnam.png'
+]
+
 
 class TestFileOperations(unittest.TestCase):
-	def test_listFiles(self):
-		result = pholof.listFiles([], [])
-		self.assertEqual(result, [])
+    def test_listImageFiles(self):
+        currentDirectory = os.getcwd()  # eg /home/user/projects/pholof
+        testFileDirectory = os.path.join(currentDirectory, "test/files/")
+        imageExts = [".jpg", ".gif", ".png", ".jpeg"]
+        fullImagePaths = [
+            os.path.join(testFileDirectory, file) for file in imageFiles
+        ]
+        result = utils.listFiles(testFileDirectory, imageExts)
+        self.assertEqual(result, fullImagePaths)
 
 
 class TestSum(unittest.TestCase):
-
     def test_sum(self):
         self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
 
     def test_sum_tuple(self):
         self.assertEqual(sum((1, 2, 2)), 5, "Should be 6")
+
 
 if __name__ == '__main__':
     unittest.main()
